69e58cdb11e1d188edf812354f22e2fc45d6fa39
Tatum13 <tatumdv2001@gmail.com>
2022-06-09 11:57:47 +0200

    Bezig met de Z as draaien

--- Muse/Assets/Scripts/InputSystem/InputParse.cs
+++ Muse/Assets/Scripts/InputSystem/InputParse.cs
@@ -10,7 +10,6 @@ public class InputParse : MonoBehaviour
     private LevelRotate _levelRotate;
     private Gravity _gravity;
     public bool _isRotating;
-    public bool _canGrab;
 
     private void Start()
     {
@@ -20,25 +19,12 @@ public class InputParse : MonoBehaviour
         _control = new FPControl(); // Maakt nieuwe controls aan.
         _inputControls = _control.PlayerControls; // Maakt een instantie van de knoppen die zijn aangemaakt?
         _levelRotate._inputControls = _inputControls;
-
         _inputControls.Gravity.performed += _gravity.TestGravity;
-
         _inputControls.Rotate.performed += _levelRotate.Pressed;
-
-        _inputControls.StartRotating.performed += _ => _isRotating = true;
-        _inputControls.StartRotating.canceled += _ => _isRotating = false; // Wanneer je het niet ingedrukt houd dan mag niet rotaten.
         _inputControls.Enable();//Functie die zegt dat die de inputcontrols mag gebruiken.
     }
     private void Update()
     {
-        _movementPlayer.Walking(_inputControls.Walking.ReadValue<Vector2>());//Kijkt in inputcontrols of die walking heeft daarna zoekt die naar de value van vector 2.
-        //_levelRotate.RotateLvl(_inputControls.LevelRotate.ReadValue<Vector3>());
-        /*
-        if (_isRotating)
-        {
-            _levelRotate.Rotate(_inputControls.MousePosition.ReadValue<Vector2>(), _inputControls.MouseDelta.ReadValue<Vector2>());
-        }
-        */
-        
+        _movementPlayer.Walking(_inputControls.Walking.ReadValue<Vector2>());//Kijkt in inputcontrols of die walking heeft daarna zoekt die naar de value van vector 2.     
     }
 }
