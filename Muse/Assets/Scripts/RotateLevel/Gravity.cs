91bacaf95e4e72b60042ca66793eaac7938f57cb
Tatum13 <tatumdv2001@gmail.com>
2022-06-16 14:08:53 +0200

    Level 3 toegevoegd

---
+++ Muse/Assets/Scripts/RotateLevel/Gravity.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gravity : MonoBehaviour
+{
+	[SerializeField] private Rigidbody rb;
+	private LevelRotate turn;
+
+	void Start()
+	{
+		turn = GameObject.Find("Level").GetComponent<LevelRotate>();
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = true;
+	}
+	public void Grav()
+	{
+		if (turn._isTurning == true)
+		{
+			rb.useGravity = false;
+			rb.velocity = Vector3.zero;
+		}
+		else
+		{
+			rb.useGravity = true;
+		}
+	}
+
+}
